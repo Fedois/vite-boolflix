bab33707cd7e0cd94f04195665b29a78f996f486
Fedois <fedefox14@gmail.com>
2023-02-08 14:51:06 +0100

    inserito serie-tv

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,13 +12,14 @@ export default {
 
 <template>
     <main>
-       <div class="container">
-            <div class="my-cards row row-cols-4">
-                <div class="my-card col" v-for="(film, index) in store.listFilm">
+       <div class="my-container border">
+        <h2>FILM</h2>
+            <div class="my-cards-films row">
+                <div class="my-card-film col" v-for="(film, index) in store.listFilm">
                     <div class="poster">
                         <img src="" alt="">
                     </div>
-                    <div class="info">
+                    <div class="info-film">
                         <h5 class="title">titolo:
                             <p class="d-inline-block">{{ film.title }}</p>
                         </h5>
@@ -34,9 +35,42 @@ export default {
                     </div>
                 </div>
             </div>
+
+            <h2>SERIE-TV</h2>
+             <div class="my-cards-films row">
+                <div class="my-card-film col" v-for="(serie, index) in store.listSerie">
+                    <div class="poster">
+                        <img src="" alt="">
+                    </div>
+                    <div class="info-film">
+                        <h5 class="title">titolo:
+                            <p class="d-inline-block">{{ serie.name }}</p>
+                        </h5>
+                        <h5 class="original-title">titolo originale:
+                            <p class="d-inline-block">{{ serie.original_name }}</p>
+                        </h5>
+                        <h5 class="languages">lingua:
+                            <p class="d-inline-block">{{ serie.original_language }}</p>
+                        </h5>
+                        <h5 class="vote">voto:
+                            <p class="d-inline-block">{{ serie.vote_average }}</p>
+                        </h5>
+                    </div>
+                </div>
+            </div>
        </div> 
     </main>
 </template>
 
 <style lang="scss" scoped>
+.my-container{
+    width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+
+    .my-cards-films{
+        width: 100%;
+        scroll-behavior: auto;
+    }
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     listFilm: [],
+    listSerie: [],
     inputNameValue: '',
 })
