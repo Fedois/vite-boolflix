4a429f725764f1efec62a3f6bb0f8ce1eda4baf1
Fedois <fedefox14@gmail.com>
2023-02-08 15:27:43 +0100

    aggiunta immagini lingua

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,7 +13,7 @@ export default {
 <template>
     <main>
        <div class="container">
-        <h2>FILM</h2>
+        <h2 class="mt-5">FILM</h2>
             <div class="my-cards-films row row-cols-4">
                 <div class="my-card-film" v-for="(film, index) in store.listFilm">
                     <div class="poster">
@@ -26,7 +26,16 @@ export default {
                         <h5 class="original-title">titolo originale:
                             <p class="d-inline-block">{{ film.original_title }}</p>
                         </h5>
-                        <h5 class="languages">lingua:
+                        <h5 v-if="film.original_language == 'en'" class="languages">lingua:
+                            <img src="../assets/Flag_of_the_United_Kingdom_(3-5).svg.png" alt="en">
+                        </h5>
+                        <h5 v-if="film.original_language == 'it'" class="languages">lingua:
+                            <img src="../assets/Flag_of_Italy.svg.png" alt="it">
+                        </h5>
+                        <h5 v-if="film.original_language == 'fr'" class="languages">lingua:
+                            <img src="../assets/Francia.jpg" alt="fr">
+                        </h5>
+                        <h5 v-else class="languages">lingua:
                             <p class="d-inline-block">{{ film.original_language }}</p>
                         </h5>
                         <h5 class="vote">voto:
@@ -36,21 +45,24 @@ export default {
                 </div>
             </div>
 
-            <h2>SERIE-TV</h2>
-             <div class="my-cards-films row row-cols-4">
-                <div class="my-card-film col" v-for="(serie, index) in store.listSerie">
+            <h2 class="mt-5">SERIE-TV</h2>
+             <div class="my-cards-series row row-cols-4">
+                <div class="my-card-serie col" v-for="(serie, index) in store.listSerie">
                     <div class="poster">
                         <img src="" alt="">
                     </div>
-                    <div class="info-film">
+                    <div class="info-serie">
                         <h5 class="title">titolo:
                             <p class="d-inline-block">{{ serie.name }}</p>
                         </h5>
                         <h5 class="original-title">titolo originale:
                             <p class="d-inline-block">{{ serie.original_name }}</p>
                         </h5>
-                        <h5 class="languages">lingua:
-                            <p class="d-inline-block">{{ serie.original_language }}</p>
+                        <h5 v-if="serie.original_language == 'en'" class="languages">lingua:
+                            <img src="../assets/Flag_of_the_United_Kingdom_(3-5).svg.png" alt="en">
+                        </h5>
+                        <h5 v-else class="languages">lingua:
+                            <p class="d-inline-block">{{ film.original_language }}</p>
                         </h5>
                         <h5 class="vote">voto:
                             <p class="d-inline-block">{{ serie.vote_average }}</p>
@@ -63,4 +75,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+img{
+    width: 20px;
+}
 </style>
